6caff7b3f07539f5e9bafd667a9f1ad7cc28f80b
Avanish Patel <apatel@solstice.com>
2018-08-13 13:23:12 -0500

    final version added

--- src/main/java/com/solstice/ecommerce/service/AddressService.java
+++ src/main/java/com/solstice/ecommerce/service/AddressService.java
@@ -2,7 +2,12 @@ package com.solstice.ecommerce.service;
 
 import com.solstice.ecommerce.model.Address;
 import com.solstice.ecommerce.repository.AddressRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AddressService {
 
     private AddressRepository addressRepository;
@@ -11,8 +16,28 @@ public class AddressService {
         this.addressRepository = addressRepository;
     }
 
-    public Address getAddressById(long id) {
+    public Optional<Address> getAddressById(long id) {
 
-        return addressRepository.getOne(id);
+        return addressRepository.findById(id);
+    }
+
+    public List<Address> getAllAddresses() {
+
+        return addressRepository.findAll();
+    }
+
+    public void addAddress(Address address) {
+
+        addressRepository.save(address);
+    }
+
+    public void updateAddress(Long id, Address address) {
+
+        addressRepository.save(address);
+    }
+
+    public void deleteAddressById(long id) {
+
+        addressRepository.deleteById(id);
     }
 }
